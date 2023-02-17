a9453037b9665d6bc3beda9bfab76c0f72afbf20
NomanSalhab <nomansalhab8516@gmail.com>
2023-02-17 02:51:35 +0200

    finished Third Video, Related To Data Binding and Validation.

--- controller/video-controller.go
+++ controller/video-controller.go
@@ -3,19 +3,25 @@ package controller
 import (
 	"github.com/NomanSalhab/go_gin_course/entity"
 	"github.com/NomanSalhab/go_gin_course/service"
+	"github.com/NomanSalhab/go_gin_course/validators"
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 type VideoController interface {
 	FindAll() []entity.Video
-	Save(ctx *gin.Context) entity.Video
+	Save(ctx *gin.Context) error /*entity.Video*/
 }
 
 type controller struct {
 	service service.VideoService
 }
 
+var validate *validator.Validate
+
 func New(service service.VideoService) VideoController {
+	validate = validator.New()
+	validate.RegisterValidation("is-cool", validators.ValidateCoolTitle)
 	return &controller{
 		service: service,
 	}
@@ -25,9 +31,18 @@ func (c *controller) FindAll() []entity.Video {
 	return c.service.FindAll()
 }
 
-func (c *controller) Save(ctx *gin.Context) entity.Video {
+func (c *controller) Save(ctx *gin.Context) error /*entity.Video*/ {
 	var video entity.Video
-	ctx.BindJSON(&video)
+	err := ctx.ShouldBindJSON(&video)
+	if err != nil {
+		return err
+	}
+
+	err = validate.Struct(video)
+	if err != nil {
+		return err
+	}
+
 	c.service.Save(video)
-	return video
+	return nil /*video*/
 }
--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io"
+	"net/http"
 	"os"
 
 	"github.com/NomanSalhab/go_gin_course/controller"
@@ -48,7 +49,12 @@ func main() {
 	})
 
 	server.POST("/videos", func(ctx *gin.Context) {
-		ctx.JSON(200, VideoController.Save(ctx))
+		err := VideoController.Save(ctx)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		} else {
+			ctx.JSON(http.StatusOK, gin.H{"message": "Video Input is Valid!"})
+		}
 	})
 
 	server.Run(":8016")
